2241abc42e62c9c26d5a9d4d5fbccf530d27a935
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2024-02-28 23:55:13 -0300

    Update ex003.py

---
+++ Mundo 01/ex003.py
@@ -0,0 +1,4 @@
+num1 = int(input('Digite seu primeiro número: '))
+num2 = int(input('Digite seu segundo número: '))
+s = num1 + num2
+print('A soma entre o número {} e o número {} é igual a {}!'.format(num1, num2, s))
